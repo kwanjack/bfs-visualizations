c2b5bea196d9d27b43cb071252f15ce461e45f2b
Jack Kwan <14180501+kwanjack@users.noreply.github.com>
2024-08-14 12:42:08 -0700

    Update slidingPuzzle.tsx

--- bfs-visualizations/app/widgets/slidingPuzzle.tsx
+++ bfs-visualizations/app/widgets/slidingPuzzle.tsx
@@ -1,25 +1,58 @@
+import React, { useState } from 'react';
 
-let boardState = [[1,2,3],[4, null, 5]];
 
 
 export default function SlidingPuzzle() {
+    const [lastClicked, setLastClicked] = useState([-1,-1]);
+    const [secondLastClicked, setSecondLastClicked] = useState([-1,-1]);
+    const [board, setBoard] = useState([[1,2,3], [4,-1,5]]);
+
+    const [exampleNumber, setExampleNumber] = useState(69);
 
     const renderCell = (row: any, col: any) => {
-        if (boardState[row][col] == null) {
+        if (board[row][col] == null) {
             return <div> ( ) </div>;
         }
-        return <div>{boardState[row][col]}</div>;
+        return <div>{board[row][col]}</div>;
+    }
+
+    const onCellClicked = (row: any, col: any) => {
+        
+        let lastClickedR = lastClicked[0];
+        let lastClickedC = lastClicked[1];
+
+        setLastClicked([row, col]);
+        setSecondLastClicked([lastClickedR, lastClickedC])
+
+        if (lastClickedR !== -1) { // cheesey way of saying "i've clcked 2 things before"
+            swap([lastClickedR, lastClickedC], [row, col]);
+        }
+    }
+
+
+    const swap = (firstPair: Array<number>, secondPair: Array<number>) => {
+        let newResult = [[...board[0]],  [...board[1]]];
+
+        newResult[firstPair[0]][firstPair[1]] = board[secondPair[0]][secondPair[1]];
+        newResult[secondPair[0]][secondPair[1]] = board[firstPair[0]][firstPair[1]];
+
+        setBoard(newResult);
     }
 
 
     return (
         <div className="grid grid-cols-3">
-            <div>{ renderCell(0, 0) }</div>
-            <div>{ renderCell(0, 1) }</div>
-            <div>{ renderCell(0, 2) }</div>
-            <div>{ renderCell(1, 0) }</div>
-            <div>{ renderCell(1, 1) }</div>
-            <div>{ renderCell(1, 2) }</div>
+            {console.log("lastclicked: " + lastClicked)}   
+            {console.log("secondlast: " + secondLastClicked)}    
+ 
+
+            <div onClick={() => onCellClicked(0, 0)}>{ renderCell(0, 0) }</div>
+            <div onClick={() => onCellClicked(0, 1)}>{ renderCell(0, 1) }</div>
+            <div onClick={() => onCellClicked(0, 2)}>{ renderCell(0, 2) }</div>
+            <div onClick={() => onCellClicked(1, 0)}>{ renderCell(1, 0) }</div>
+            <div onClick={() => onCellClicked(1, 1)}>{ renderCell(1, 1) }</div>
+            <div onClick={() => onCellClicked(1, 2)}>{ renderCell(1, 2) }</div>
+            <div> My secret number is {exampleNumber} </div>
         </div>
     );
   }
